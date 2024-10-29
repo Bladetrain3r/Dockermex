e7b2d4688debc7454c4b35b8ad24137a047beae0
Ziggy <Bladetrain3r@gmail.com>
2024-10-29 14:56:49 +0200

    scripts

--- africa.sh
+++ africa.sh
@@ -2,5 +2,6 @@
 git reset --hard
 git pull
 sed -i 's/Hells Keep/Hells Keep - Africa/g' ./configs/*
+sed -i 's/localhost:8080/odamex.zerofuchs.co.za:8080/g' ./configs/*
 docker-compose -f docker-compose-production.yml down
 docker-compose -f docker-compose-production.yml up
--- brazil.sh
+++ brazil.sh
@@ -2,5 +2,6 @@
 git reset --hard
 git pull
 sed -i 's/Hells Keep/Hells Keep - Brazil/g' ./configs/*
+sed -i 's/localhost:8080/odamex.br.zerofuchs.net:8080/g' ./configs/*
 docker-compose -f docker-compose-production.yml down
 docker-compose -f docker-compose-production.yml up
