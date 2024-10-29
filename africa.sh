7fa43c85a829ab8c8f72fe3f69e33b1be27ed0be
Ziggy <Bladetrain3r@gmail.com>
2024-10-29 17:29:52 +0200

    chmod

--- africa.sh
+++ africa.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git reset --hard
 git pull
+chmod +x *.sh
 sed -i 's/Hells Keep/Hells Keep - Africa/g' ./configs/*
 sed -i 's/localhost:8080/odamex.zerofuchs.co.za:8080/g' ./configs/*
 docker-compose -f docker-compose-personal.yml down
--- brazil.sh
+++ brazil.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git reset --hard
 git pull
+chmod +x *.sh
 sed -i 's/Hells Keep/Hells Keep - Brazil/g' ./configs/*
 sed -i 's/localhost:8080/odamex.br.zerofuchs.net:8080/g' ./configs/*
 docker-compose -f docker-compose-production.yml down
