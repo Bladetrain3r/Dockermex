93f1492e38f3feede39ec7e71ea3c06f69685c0a
Ziggy <Bladetrain3r@gmail.com>
2024-10-31 21:47:27 +0200

    .

--- africa.sh
+++ africa.sh
@@ -3,6 +3,6 @@ git reset --hard
 git pull
 chmod +x *.sh
 sed -i 's/Hells Keep/Hells Keep - Africa/g' ./configs/*
-sed -i 's/localhost:8080/odamex.zerofuchs.co.za:8080/g' ./configs/*
+sed -i 's/localhost:8080/example.co.za:443/g' ./configs/*
 docker-compose -f docker-compose-personal.yml down
 docker-compose -f docker-compose-personal.yml up
